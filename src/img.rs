7b17f2622f25f42b56c6c710fc35b8fa07da1edf
Atsushi Toda <todaatsushi0@gmail.com>
2023-06-03 17:12:59 +0100

    Build image store from args

--- src/img.rs
+++ src/img.rs
@@ -1,7 +1,14 @@
 use crate::call_args::get_args;
 use crate::exceptions::FileErr;
 use image::DynamicImage;
-use std::{collections::HashMap, fmt::Display, path::PathBuf, str::FromStr};
+use std::{
+    collections::HashMap,
+    ffi::OsStr,
+    fmt::Display,
+    fs::{self, DirEntry, ReadDir},
+    path::{Path, PathBuf},
+    str::FromStr,
+};
 
 enum ImageFile {
     PNG,
@@ -39,23 +46,72 @@ fn get_img(img_path: &PathBuf) -> Option<DynamicImage> {
     }
 }
 
+fn is_image(file: &DirEntry) -> bool {
+    let path = file.path();
+    let p = Path::new(&path);
+    let ext = p.extension().and_then(OsStr::to_str);
+    match ext {
+        Some(e) => {
+            let image_ext = ImageFile::from_str(e);
+            match image_ext {
+                Ok(_) => true,
+                Err(_) => false,
+            }
+        }
+        None => false,
+    }
+}
+
+fn build_map_from_dir(paths: ReadDir) -> HashMap<String, Option<DynamicImage>> {
+    let mut store: HashMap<String, Option<DynamicImage>> = HashMap::new();
+    paths.into_iter().for_each(|p| {
+        let path = p.unwrap();
+        match is_image(&path) {
+            true => match get_img(&path.path()) {
+                Some(img) => {
+                    let path_str = path.path().to_string_lossy().into_owned();
+                    store.insert(path_str, Some(img));
+                }
+                None => (),
+            },
+            false => (),
+        }
+    });
+    store
+}
+
+fn build_map_from_file(
+    file_path: PathBuf,
+) -> Result<HashMap<String, Option<DynamicImage>>, FileErr> {
+    let mut store: HashMap<String, Option<DynamicImage>> = HashMap::new();
+    match get_img(&file_path) {
+        Some(img) => {
+            let path = file_path.to_str().unwrap().to_string();
+            store.insert(path, Some(img));
+        }
+        None => return Err(FileErr::CouldntRead),
+    };
+    Ok(store)
+}
+
 pub fn open_images() -> Result<HashMap<String, Option<DynamicImage>>, FileErr> {
     match get_args() {
         Ok(args) => {
             let arg = &args[1];
-            let mut store: HashMap<String, Option<DynamicImage>> = HashMap::new();
             let target = PathBuf::from(arg);
+            let store;
 
             if target.is_dir() {
-                ()
+                let files = fs::read_dir(target.to_str().unwrap());
+                store = match files {
+                    Ok(paths) => build_map_from_dir(paths),
+                    Err(_e) => return Err(FileErr::CouldntRead),
+                };
             } else if target.is_file() {
-                match get_img(&target) {
-                    Some(img) => {
-                        let path = target.to_str().unwrap().to_string();
-                        store.insert(path, Some(img));
-                    }
-                    None => return Err(FileErr::CouldntRead),
-                }
+                store = match build_map_from_file(target) {
+                    Ok(s) => s,
+                    Err(e) => return Err(e),
+                };
             } else {
                 println!("Image error. File might not exist so check that the file exists at that location. Otherwise, try another file.");
                 return Err(FileErr::CouldntRead);

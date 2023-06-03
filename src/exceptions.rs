96fc9c86313889424789ba40d4ec9e7ba741126c
Atsushi Toda <todaatsushi0@gmail.com>
2023-06-03 19:15:30 +0100

    Fix error message

--- src/exceptions.rs
+++ src/exceptions.rs
@@ -15,7 +15,7 @@ impl Display for ArgErr {
                 println!("Got invalid number of arguments ({})", num - 1);
                 write!(
                     f,
-                    "You need to give 3 arguments - the file path, the max number of colors, and the destination path (incl the filename)."
+                    "You need to give 3 arguments - the file path, the max number of colors, and the destination folder (no filenames)."
                 )
             }
             ArgErr::BadColorNumber(num) => {
